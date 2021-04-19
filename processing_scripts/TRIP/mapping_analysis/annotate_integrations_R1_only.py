a4eed6c1b9b1b4e8d1584f929bc74566c4f8aae5
claricehong <28317066+claricehong@users.noreply.github.com>
2021-04-19 13:44:04 -0500

    Updated annotate_integrations script with comments/modifications.

--- processing_scripts/TRIP/mapping_analysis/annotate_integrations_R1_only.py
+++ processing_scripts/TRIP/mapping_analysis/annotate_integrations_R1_only.py
@@ -6,19 +6,15 @@ import pysam
 import collections
 import itertools
 
-parser = argparse.ArgumentParser(
-    description = 'Read bam file and output barcode with annotated locations')
-parser.add_argument(
-    'bam', help = 'bamfile')
-parser.add_argument(
-    'insulator_bcs', help = 'insulator BC file')
-parser.add_argument(
-    'output', help = 'output file name')
+parser = argparse.ArgumentParser(description = 'Read bam file and output barcode with annotated locations')
+parser.add_argument('bam', help = 'bamfile')
+parser.add_argument('promoter_bcs', help = 'promoter BC file')
+parser.add_argument('output', help = 'output file name')
 args = parser.parse_args()
 
-# create barcodes with one substitution because the mapping is full of errors
+# create barcodes with one substitution
 def generate_one_substitution(seq):
-
+    """Create all barcodes one hamming distance from the current barcode"""
     out = [seq]
 
     for position, nt in enumerate(seq):
@@ -31,7 +27,7 @@ def generate_one_substitution(seq):
     return out
 
 def rev_comp(seq):
-    
+    """Generates reverse complement of DNA string"""
     new_seq = ''
     conversion = {'A': 'T', 'T': 'A', 'G':'C', 'C':'G'}
     
@@ -41,7 +37,7 @@ def rev_comp(seq):
     return new_seq
 
 def check_Ns(seq):
-
+    """Check whether DNA string contains Ns"""
     valid = ['A', 'T', 'C', 'G']
 
     for i in seq:
@@ -51,60 +47,23 @@ def check_Ns(seq):
     return True
 
 def get_strand(read):
-    
+    """Get the strand of the mapped read"""
     if read.is_reverse:
         return '-'
     
     return '+'
 
-
-d_insulator_bcs = {}
-
-with open(args.insulator_bcs, 'r') as f:
-    header = f.readline()
-    for line in f:
-        name, bc = line.strip().split('\t')
-        related_bcs = generate_one_substitution(bc)
-        for each_bc in related_bcs:
-            d_insulator_bcs[each_bc] = name
-
-bamfile = pysam.AlignmentFile(args.bam, "rb")
-
-count_mapped_barcodes = 0
-count_matched_barcodes = 0
-all_locations = collections.defaultdict(list)
-
-for read in bamfile:
-    if not read.is_unmapped:
-        if not read.is_secondary:
-            count_mapped_barcodes += 1
-            insulator_bc = read.query_name.split(':')[-2]
-            trip_bc = read.query_name.split(':')[-1]
-            chromosome = read.reference_name
-            location = str(read.get_reference_positions()[0])
-            if insulator_bc in d_insulator_bcs.keys() and check_Ns(trip_bc) == True:
-                count_matched_barcodes += 1
-                insulator = d_insulator_bcs[insulator_bc]
-                strand = get_strand(read)
-                all_locations[(insulator, rev_comp(trip_bc))].append((chromosome, location, strand))
-
-all_locations = {bcs: locations for bcs, locations in all_locations.items() if len(locations) > 2}
-
-print('Number of mapped barcodes: ' + str(count_mapped_barcodes))
-print('Number of matched barcodes: ' + str(count_matched_barcodes))
-
-barcode_locations = {}
-barcode_locations2 = {}
-
 def check_percentage(location, total, top_count):
-
+    """
+    Checks that a given location represents more than 10% of the total reads and is more than 20% of the top location for a given barcode pair
+    """
     if location[1]/total > 0.1 and location[1]*5 > top_count:
         return 'over'
     else:
         return 'below'
 
 def check_all_percentages(all_locations, total, top_count):
-
+    """Filters locations for barcode-pairs that have more than one location mapped"""
     over_results = []
 
     for location in all_locations:
@@ -117,12 +76,56 @@ def check_all_percentages(all_locations, total, top_count):
         return over_results
 
 def compare_locations(loc1, loc2):
-
+    """Checks whether locations are within 1kb of each other"""
     if loc1[0] == loc2[0] and loc1[2] == loc2[2] and abs(int(loc1[1]) - int(loc2[1])) < 1000:
         return 'close'
     else:
         return 'far'
 
+
+d_promoter_bcs = {}
+
+# read promoter barcodes
+with open(args.promoter_bcs, 'r') as f:
+    header = f.readline()
+    for line in f:
+        name, bc = line.strip().split('\t')
+        related_bcs = generate_one_substitution(bc)
+        for each_bc in related_bcs:
+            d_promoter_bcs[each_bc] = name
+
+# read bam file
+bamfile = pysam.AlignmentFile(args.bam, "rb")
+
+count_mapped_barcodes = 0
+count_matched_barcodes = 0
+all_locations = collections.defaultdict(list)
+
+# iterate through each mapped read
+for read in bamfile:
+    if not read.is_unmapped:
+        if not read.is_secondary:
+            count_mapped_barcodes += 1
+            promoter_bc = read.query_name.split(':')[-2]
+            trip_bc = read.query_name.split(':')[-1]
+            chromosome = read.reference_name
+            location = str(read.get_reference_positions()[0])
+            
+            # assign location to promoter/trip barcode pair 
+            if promoter_bc in d_promoter_bcs.keys() and check_Ns(trip_bc) == True:
+                count_matched_barcodes += 1
+                promoter = d_promoter_bcs[promoter_bc]
+                strand = get_strand(read)
+                all_locations[(promoter, rev_comp(trip_bc))].append((chromosome, location, strand))
+
+# ensure that there are at least 3 reads supporting each location
+all_locations = {bcs: locations for bcs, locations in all_locations.items() if len(locations) > 2}
+
+print('Number of mapped barcodes: ' + str(count_mapped_barcodes))
+print('Number of matched barcodes: ' + str(count_matched_barcodes))
+
+barcode_locations = {}
+barcode_locations2 = {}
 
 for bcs, locations in all_locations.items():
     count_items = collections.Counter(locations)
